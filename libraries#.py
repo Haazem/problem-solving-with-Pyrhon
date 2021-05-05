21f820654358f2b4d7c8eda7cf1773b5f4b48763
HAZEM <73290269+Haazem@users.noreply.github.com>
2021-05-05 21:59:37 +0200

    Add files via upload

---
+++ libraries#.py
@@ -0,0 +1,77 @@
+
+
+from itertools import permutations , combinations , accumulate , count
+#count method  To infinity loop
+import heapq
+from collections import deque , defaultdict , Counter
+from sys import stdin , stdout
+import math
+from bisect import *
+from functools import *    #Don't Know
+from math import radians , degrees , pi , hypot
+from fractions import Fraction
+import cmath # lib for complex Numbers
+from copy import deepcopy
+from collections import deque
+from itertools import product
+import numpy as np
+import statistics
+
+
+#divmod()
+#from codecs import encode ##method rot_13
+#dx = [-1 , 1 , 0 , 0  ]
+#dy = [0 , 0  , 1  , - 1]
+#import re ###re.split #method
+#dx = [0 , 0 , 1 , -1, 1 , -1 , 1 , -1]#Kingpath
+#dy = [-1 , 1 , 0 , 0 , -1 , -1 , 1 , 1]#kingPath
+#visited = [[False for i in range(m)] for j in range(n)] in grid
+#from heapq import heappop , heappush
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
